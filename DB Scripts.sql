cbd01bb7dfe48a04d30ed8d93e8ed2cc373cae83
Strong <mistrong@caswom1078767.ca.deloitte.com>
2018-09-07 13:45:48 -0400

    Events page working

--- DB Scripts.sql
+++ DB Scripts.sql
@@ -11,20 +11,24 @@ CREATE TABLE `Matchup`.`Profile` (
   `Language` VARCHAR(45) NULL,
   `Major` VARCHAR(45) NULL,
   `Local` INT NULL,
+  `Email` VARCHAR(45) NULL,
+  `Phone` VARCHAR(45) NULL,
+  `Password` VARCHAR(45) NULL,
+  
   PRIMARY KEY (`Profile_ID`),
-  UNIQUE INDEX `Profile_ID_UNIQUE` (`Profile_ID` ASC) VISIBLE);
+  UNIQUE INDEX `Profile_ID_UNIQUE` (`Profile_ID` ASC));
 
   CREATE TABLE `Matchup`.`Topics` (
   `Topic_ID` VARCHAR(45) NOT NULL,
   `Topic` VARCHAR(45) NULL,
   PRIMARY KEY (`Topic_ID`),
-  UNIQUE INDEX `Topic_ID_UNIQUE` (`Topic_ID` ASC) VISIBLE);
+  UNIQUE INDEX `Topic_ID_UNIQUE` (`Topic_ID` ASC));
 
 CREATE TABLE `Matchup`.`Interests` (
   `Profile_ID` VARCHAR(45) NOT NULL,
   `Topic_ID` VARCHAR(45) NOT NULL,
-  INDEX `Interests_Profile_ID_idx` (`Profile_ID` ASC) VISIBLE,
-  INDEX `Interests_Topic_ID_idx` (`Topic_ID` ASC) VISIBLE,
+  INDEX `Interests_Profile_ID_idx` (`Profile_ID` ASC),
+  INDEX `Interests_Topic_ID_idx` (`Topic_ID` ASC),
   CONSTRAINT `Interests_Profile_ID`
     FOREIGN KEY (`Profile_ID`)
     REFERENCES `Matchup`.`Profile` (`Profile_ID`)
@@ -43,9 +47,9 @@ CREATE TABLE `Matchup`.`Matched` (
   `Person2` VARCHAR(45) NULL,
   `Strength` INT NULL,
   PRIMARY KEY (`Match_ID`),
-  UNIQUE INDEX `Match_ID_UNIQUE` (`Match_ID` ASC) VISIBLE,
-  INDEX `Match_Person1_idx` (`Person1` ASC) VISIBLE,
-  INDEX `Match_Person2_idx` (`Person2` ASC) VISIBLE,
+  UNIQUE INDEX `Match_ID_UNIQUE` (`Match_ID` ASC),
+  INDEX `Match_Person1_idx` (`Person1` ASC),
+  INDEX `Match_Person2_idx` (`Person2` ASC),
   CONSTRAINT `Match_Person1`
     FOREIGN KEY (`Person1`)
     REFERENCES `Matchup`.`Profile` (`Profile_ID`)
@@ -60,8 +64,8 @@ CREATE TABLE `Matchup`.`Matched` (
 CREATE TABLE `Matchup`.`Matched_On` (
   `Matched_ID` VARCHAR(45) NOT NULL,
   `Topic_ID` VARCHAR(45) NOT NULL,
-  INDEX `Matched_On_MatchID_idx` (`Matched_ID` ASC) VISIBLE,
-  INDEX `Matched_On_TopicID_idx` (`Topic_ID` ASC) VISIBLE,
+  INDEX `Matched_On_MatchID_idx` (`Matched_ID` ASC),
+  INDEX `Matched_On_TopicID_idx` (`Topic_ID` ASC),
   CONSTRAINT `Matched_On_MatchID`
     FOREIGN KEY (`Matched_ID`)
     REFERENCES `Matchup`.`Matched` (`Match_ID`)
@@ -71,7 +75,7 @@ CREATE TABLE `Matchup`.`Matched_On` (
     FOREIGN KEY (`Topic_ID`)
     REFERENCES `Matchup`.`Topics` (`Topic_ID`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION); 
 
 CREATE TABLE `Matchup`.`Event` (
   `Event_ID` VARCHAR(45) NOT NULL,
@@ -79,14 +83,14 @@ CREATE TABLE `Matchup`.`Event` (
   `Location` VARCHAR(45) NULL,
   `Time` DATETIME NULL,
   PRIMARY KEY (`Event_ID`),
-  UNIQUE INDEX `Event_ID_UNIQUE` (`Event_ID` ASC) VISIBLE);
+  UNIQUE INDEX `Event_ID_UNIQUE` (`Event_ID` ASC));
 
 
 CREATE TABLE `Matchup`.`Event_Topics` (
   `Event_ID` VARCHAR(45) NOT NULL,
   `Topic_ID` VARCHAR(45) NOT NULL,
-  INDEX `EventTopic_Event_idx` (`Event_ID` ASC) VISIBLE,
-  INDEX `EventTopic_Topic_idx` (`Topic_ID` ASC) VISIBLE,
+  INDEX `EventTopic_Event_idx` (`Event_ID` ASC),
+  INDEX `EventTopic_Topic_idx` (`Topic_ID` ASC),
   CONSTRAINT `EventTopic_Event`
     FOREIGN KEY (`Event_ID`)
     REFERENCES `Matchup`.`Event` (`Event_ID`)
@@ -101,8 +105,8 @@ CREATE TABLE `Matchup`.`Event_Topics` (
 CREATE TABLE `Matchup`.`Attend` (
   `Event_ID` VARCHAR(45) NOT NULL,
   `Profile_ID` VARCHAR(45) NOT NULL,
-  INDEX `Attend_Event_idx` (`Event_ID` ASC) VISIBLE,
-  INDEX `Attend_Profile_idx` (`Profile_ID` ASC) VISIBLE,
+  INDEX `Attend_Event_idx` (`Event_ID` ASC),
+  INDEX `Attend_Profile_idx` (`Profile_ID` ASC),
   CONSTRAINT `Attend_Event`
     FOREIGN KEY (`Event_ID`)
     REFERENCES `Matchup`.`Event` (`Event_ID`)
@@ -114,19 +118,21 @@ CREATE TABLE `Matchup`.`Attend` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
+
 CREATE TABLE `Matchup`.`Forum` (
   `Forum_ID` VARCHAR(45) NOT NULL,
   `Name` VARCHAR(45) NULL,
   ` Created_On` DATETIME NULL,
   `Created_By` VARCHAR(45) NULL,
   PRIMARY KEY (`Forum_ID`),
-  UNIQUE INDEX `Forum_ID_UNIQUE` (`Forum_ID` ASC) VISIBLE,
-  INDEX `Forum_CreatedBy_idx` (`Created_By` ASC) VISIBLE,
+  UNIQUE INDEX `Forum_ID_UNIQUE` (`Forum_ID` ASC),
+  INDEX `Forum_CreatedBy_idx` (`Created_By` ASC),
   CONSTRAINT `Forum_CreatedBy`
     FOREIGN KEY (`Created_By`)
     REFERENCES `Matchup`.`Profile` (`Profile_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
 
 CREATE TABLE `Matchup`.`Post` (
   `Post_ID` VARCHAR(45) NOT NULL,
@@ -134,9 +140,9 @@ CREATE TABLE `Matchup`.`Post` (
   `Profile_ID` VARCHAR(45) NULL,
   `Text` VARCHAR(255) NULL,
   PRIMARY KEY (`Post_ID`),
-  UNIQUE INDEX `Post_ID_UNIQUE` (`Post_ID` ASC) VISIBLE,
-  INDEX `Post_Forum_idx` (`Forum_ID` ASC) VISIBLE,
-  INDEX `Post_Profile_idx` (`Profile_ID` ASC) VISIBLE,
+  UNIQUE INDEX `Post_ID_UNIQUE` (`Post_ID` ASC),
+  INDEX `Post_Forum_idx` (`Forum_ID` ASC),
+  INDEX `Post_Profile_idx` (`Profile_ID` ASC),
   CONSTRAINT `Post_Forum`
     FOREIGN KEY (`Forum_ID`)
     REFERENCES `Matchup`.`Forum` (`Forum_ID`)
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, json, request, jsonify, send_from_directory
 #from flask.ext.mysql import MySQL
+from collections import namedtuple
 from flaskext.mysql import MySQL
 
 
@@ -42,7 +43,61 @@ def getUser():
     results = cursor.fetchall()
     return render_template('Matches.html', data=results)
 
-    #return jsonify(results)
+@app.route('/events',methods=['GET'])
+def getEvent():
+    
+    event = namedtuple("event", "name location time strength interests")
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM Event"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    sql = """SELECT D.Name, C.Strength FROM (
+SELECT B.Event_ID as 'Event', COUNT(*) as 'Strength' FROM (
+SELECT Topic_ID from `Matchup`.`Interests` 
+WHERE Profile_ID = '1' )A
+JOIN
+(SELECT Event_ID, Topic_ID from `Matchup`.`Event_Topics` 
+) B
+ON A.Topic_ID = B.Topic_ID
+GROUP BY B.Event_ID
+ORDER BY COUNT(*) DESC) C
+JOIN
+(SELECT * FROM `Matchup`.`Event`) D
+ON C.Event = D.Event_ID;"""
+    cursor.execute(sql)
+    results2 = cursor.fetchall()
+    sql = """SELECT X.Name, Y.Topic FROM (
+SELECT C.Name, D.Topic_ID FROM (
+SELECT B.Event_ID  as 'Event' , B.Topic_ID FROM (
+SELECT Topic_ID from `Matchup`.`Interests` 
+WHERE Profile_ID = '1' )A
+JOIN
+(SELECT Event_ID, Topic_ID from `Matchup`.`Event_Topics` ) B
+ON A.Topic_ID = B.Topic_ID
+) D 
+JOIN 
+(SELECT * FROM `Matchup`.`Event` ) C
+ON D.Event = C.Event_ID) X
+JOIN 
+(SELECT * FROM `Matchup`.`Topics` ) Y
+ON X.Topic_ID = Y.Topic_ID
+ORDER BY X.NAME;"""
+    cursor.execute(sql)
+    results3 = cursor.fetchall()
+    L = []
+    for item in results2:
+        for i2 in results:
+            topics = ""
+            if (i2[1] == item[0]):
+                for i3 in results3:
+                    if (i3[0] == item[0]):
+                        topics += i3[1] + ", "
+                topics = topics[0: len(topics)-2]
+                e = event(item[0],i2[2], i2[3], item[1], topics)
+                L.append(e)
+    
+    return render_template('index.html', data=L)
 
 
 if __name__ == "__main__":
