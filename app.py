08d449497b0d6a3dfff0afc3ca3a3e5ae316efdd
Strong <mistrong@caswom1078767.ca.deloitte.com>
2018-09-10 09:41:42 -0400

    Updated script

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def getMatchedProfiles():
     #profile_id = data['Profile_Id']
     conn = mysql.connect()
     cursor = conn.cursor()
-    sql = "SELECT B.Profile_ID as 'Match', COUNT(*) as 'Strength' FROM (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests` WHERE Profile_ID = '4' )A JOIN (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests` WHERE Profile_ID <> '4') B ON A.Topic_ID = B.Topic_ID GROUP BY B.Profile_ID ORDER BY COUNT(*) DESC"
+    sql = "SELECT B.Profile_ID as 'Match', COUNT(*) as 'Strength' FROM (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests` WHERE Profile_ID = '1' )A JOIN (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests` WHERE Profile_ID <> '1') B ON A.Topic_ID = B.Topic_ID GROUP BY B.Profile_ID ORDER BY COUNT(*) DESC"
     cursor.execute(sql)
     results = cursor.fetchall()
     print(results)
@@ -82,7 +82,7 @@ def getMatchedProfiles():
         #print(results[i][0])
         #print(matchedPplResults[i][0])
     print("fas:",newresult)
-    cursor.execute("SELECT A.Person, B.Topic AS 'Topic' FROM (   SELECT B.Profile_ID as 'Person', B.Topic_ID as 'Topic' FROM (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests`  WHERE Profile_ID = '4' )A JOIN (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests`  WHERE Profile_ID <> '4') B ON A.Topic_ID = B.Topic_ID ORDER BY B.Profile_ID DESC) A  JOIN  `Matchup`.`Topics` B  ON A.Topic = B.Topic_ID ORDER BY A.Person")
+    cursor.execute("SELECT A.Person, B.Topic AS 'Topic' FROM (   SELECT B.Profile_ID as 'Person', B.Topic_ID as 'Topic' FROM (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests`  WHERE Profile_ID = '1' )A JOIN (SELECT Profile_ID, Topic_ID from `Matchup`.`Interests`  WHERE Profile_ID <> '1') B ON A.Topic_ID = B.Topic_ID ORDER BY B.Profile_ID DESC) A  JOIN  `Matchup`.`Topics` B  ON A.Topic = B.Topic_ID ORDER BY A.Person")
     matchedInterests = cursor.fetchall()
     #newlist = tuple(matchedPplResults) + tuple (matchedInterests)
     #print(newlist)
